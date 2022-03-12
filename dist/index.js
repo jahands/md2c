11cfb029bc1e9627523bd4683c537011db4b90b6
Jacob Hands <jacob@gogit.io>
2022-03-12 11:18:04 -0600

    Upgrade typescript/webpack

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,23 @@
 "use strict";
-exports.__esModule = true;
-var marked = require("marked");
+Object.defineProperty(exports, "__esModule", { value: true });
+const marked = require("marked");
 /**
  * 转义`Confluence`语法使用的特殊字符
  * @param text 将要转义的字符串
  */
-var escape = function (text) { return (text || '').replace(/(?=[{}|*!\[\]])/gm, '\\'); };
+const escape = (text) => (text || '').replace(/(?=[{}|*!\[\]])/gm, '\\');
 /**
  * 将代码语言转成`Confluence`识别的语言，他识别的语言如下：
  * actionscript3,applescript,bash,c#,cpp,css,coldfusion,delphi,diff,erl,groovy,
  * xml,java,jfx,js,php,perl,text,powershell,py,ruby,sql,sass,scala,vb,yaml(6.7)
  */
-var languages = {
+const languages = {
     html: 'xml',
     json: 'js',
     javascript: 'js',
     less: 'sass',
     'c++': 'cpp',
-    python: 'py'
+    python: 'py',
 };
 /**
  * 创建一个将`Markdown`转成`Confluence Wiki Markup`的渲染器
@@ -26,25 +26,29 @@ var languages = {
  */
 function Renderer() { }
 Object.assign(Renderer.prototype, marked.Renderer.prototype, {
-    hr: function () { return '----\n'; },
-    em: function (text) { return "_" + text + "_"; },
-    del: function (text) { return "-" + text + "-"; },
-    strong: function (text) { return "*" + text + "*"; },
-    text: function (text) { return text; },
-    link: function (href, title, text) { return text ? "[" + text + "|" + href + "]" : "[" + href + "]"; },
-    image: function (href, title, alt) { return "!" + href + "|title=" + (title || '') + ",alt=" + alt + "!"; },
-    code: function (code, lang) { return "{code:language=" + (languages[lang] || lang) + "|linenumbers=true|collapse=false}\n" + code + "\n{code}\n"; },
+    hr: () => '----\n',
+    em: (text) => `_${text}_`,
+    del: (text) => `-${text}-`,
+    strong: (text) => `*${text}*`,
+    text: (text) => text,
+    link: (href, title, text) => text ? `[${text}|${href}]` : `[${href}]`,
+    image: (href, title, alt) => `!${href}|title=${title || ''},alt=${alt}!`,
+    code: (code, lang) => `{code:language=${languages[lang] || lang}|linenumbers=true|collapse=false}\n${code}\n{code}\n`,
     // 一对大括号两边要有空格，防止用户没有添加空格导致不能识别
-    codespan: function (code) { return " {{" + escape(code) + "}} "; },
-    list: function (text, ordered) { return text.trim().replace(/^|(?<=\n+)/g, ordered ? '# ' : '* ') + '\n\n'; },
-    listitem: function (text) { return text + "\n"; },
-    table: function (header, body) { return "" + header + body; },
-    tablerow: function (cells) { return cells + "|\n"; },
-    tablecell: function (cell, options) { return options.header ? "||" + cell : "|" + cell; },
-    heading: function (text, depth) { return "h" + depth + ". " + text + "\n"; },
-    paragraph: function (text) { return text + "\n"; },
-    blockquote: function (quote) { return "{quote}" + quote + "{quote}\n"; }
+    codespan: (code) => ` {{${escape(code)}}} `,
+    list: (text, ordered) => text.trim().replace(/^|(?<=\n+)/g, ordered ? '# ' : '* ') + '\n\n',
+    listitem: (text) => `${text}\n`,
+    table: (header, body) => `${header}${body}`,
+    tablerow: (cells) => `${cells}|\n`,
+    tablecell: (cell, options) => options.header ? `||${cell}` : `|${cell}`,
+    heading: (text, depth) => `h${depth}. ${text}\n`,
+    paragraph: (text) => `${text}\n`,
+    blockquote: (quote) => `{quote}${quote}{quote}\n`,
+    // 使用`marked`自带渲染器的方法
+    // br: ()=>'\n',
+    // html: (html)=>html,
+    // checkbox: (checked)=>checked,
 });
-var renderer = new Renderer();
-exports["default"] = (function (markdown) { return marked(markdown, { renderer: renderer }); });
+const renderer = new Renderer();
+exports.default = (markdown) => marked(markdown, { renderer });
 //# sourceMappingURL=index.js.map
